bc5d903e6741c69f18131bdac9c336e51fce4a3e
Mayram <sultanovamayram4@gmail.com>
2019-10-26 09:03:22 +0600

    first commit

---
+++ task19.2.py
@@ -0,0 +1,8 @@
+num = int(input("Your weight :"))
+print ("You weight on the moon: ")
+print (num*0.165)
+i = 1
+while i <= 15:
+    print ("You have become fatter:")
+    print((num+i)*0.165)
+    i += 1
